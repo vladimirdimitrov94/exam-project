1731bb46f23330be041c5919c6738e31f5d2d452
vladimirdimitrov94 <1m2x34u@gmail.com>
2024-12-14 15:09:00 +0200

    fix userService

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,61 +1,41 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { User } from '../types/user';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject, Subscription, tap, } from 'rxjs';
+import { tap, } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class UserService implements OnDestroy{
-  private user$$ = new BehaviorSubject<User | null>(null)
-  private user$ = this.user$$.asObservable()
+export class UserService {
 
-  USER_KEY = '[user]';
   user: User | null = null;
-  userSubscription: Subscription | null = null;
 
-
-  get isLogged() : boolean {
+  get isLogged(): boolean {
     return !!this.user;
   }
 
-  constructor(private http: HttpClient) {
-    this.user$.subscribe((user) => {
-      this.user = user;
-    })
-   }
+  constructor(private http: HttpClient) { }
 
   login(email: string, password: string) {
 
     const url = "https://api.backendless.com/4D2A8539-DF52-4239-AABA-8BCDFE9BF391/4425D43C-344E-43B3-A811-D68B92E6010F/users/login"
     return this.http.post<User>(url, { login: email, password }).pipe(tap(user => {
-      this.user$$.next(user)
       this.user = user;
-      localStorage.setItem('token', user['user-token'])
     }))
   }
 
   logout() {
-    // const url = "https://api.backendless.com/4D2A8539-DF52-4239-AABA-8BCDFE9BF391/4425D43C-344E-43B3-A811-D68B92E6010F/users/logout"
-    // this.http.get(url);
+    const url = "https://api.backendless.com/4D2A8539-DF52-4239-AABA-8BCDFE9BF391/4425D43C-344E-43B3-A811-D68B92E6010F/users/logout"
+    this.http.get(url);
     this.user = null;
-    localStorage.removeItem('token')
   }
 
   register(email: string, password: string, username: string) {
     const url = "/api/Users"
 
     return this.http.post<User>(url, { email, password, username }).pipe(tap(user => {
-      this.user$$.next(user)
       this.user = user;
     }))
   }
 
-  getUser(){
-    return this.user;
-  }
-
-  ngOnDestroy(): void {
-    this.userSubscription?.unsubscribe();
-  }
 }
