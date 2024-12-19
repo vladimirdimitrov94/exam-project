d4f70e08f16d1fec0fe768b53a88c007bff7fff1
vladimirdimitrov94 <1m2x34u@gmail.com>
2024-12-19 11:30:40 +0200

    test withCredentials

--- src/app/app.interceptor.ts
+++ src/app/app.interceptor.ts
@@ -14,7 +14,8 @@ export const appInterceptor: HttpInterceptorFn = (req, next) => {
 
   if (req.url.startsWith(API)) {
     req = req.clone({
-      url: req.url.replace(API, apiUrl)
+      url: req.url.replace(API, apiUrl),
+      withCredentials: true
     })
   }
 
